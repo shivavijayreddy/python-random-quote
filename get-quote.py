4ee4f2eb067c1bacb40feb7f536b78ff71dfc28c
shivavijayreddy <55069065+shivavijayreddy@users.noreply.github.com>
2021-06-04 04:00:44 -0700

    added quotes programmaticaly, print mulitple lines, remove \n

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,33 @@ import random
 def primary():
   #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
+  f = open("quotes.txt", "a")
+  f.write("Now the file has more content!") #add quotes programmatically
+  f = open("quotes.txt", "r")
   quotes = f.readlines()
   f.close()
-  last=13   #last = len(quotes) - 1
-  rnd= random.randint(0,last)
-  print(quotes[rnd])
+  #last=13   #last = len(quotes) - 1
+  #rnd1= random.randint(0,last)
+  #rnd2= random.randint(0,last)
+  quotesE = [i.strip('\n') if type(i)== str else str(i) for i in quotes]
+   #remove \n from each item in list
 
+
+
+
+
+
+  print(quotesE[0], end = ' ')
+  #print(quotes[rnd2])
+  print(quotesE[1])
+
+  print(quotesE[0])
+  #print(quotes[rnd2])
+  print(quotesE[1])
+
+  #print(type(quotes))
+
+  #print("hello", end=" ")
+  #print("world")
 if __name__== "__main__":
   primary()
